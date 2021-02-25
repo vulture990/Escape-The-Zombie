1d0a3dc49a41030d91e83689c98ef43f39be3b19
Mohamedsalbi <73004418+vulture990@users.noreply.github.com>
2021-02-25 23:54:32 +0100

    nightyy commitxD

--- src/com.vulture/entity/Tile.java
+++ src/com.vulture/entity/Tile.java
@@ -1,14 +1,23 @@
 package com.vulture.entity;
 
 
+//import com.vulture.Assets.AssetStuff;
+
+import com.vulture.Assets.AssetStuff;
+
 public class Tile  {
     private Ground gr;
     private Player player;
+    private AssetStuff assets;
+  //  private AssetStuff objects;
     public Tile(Ground gr){
         this.gr = gr;
     }
     public  Ground getGround(){
         return gr;
+    }
+    public void setGround(Ground gr){
+          this.gr=gr;
     }
     public Player getPlayer(){
         return player;
@@ -17,5 +26,11 @@ public class Tile  {
         this.player= player;
     }
 
+    public void setAssets(AssetStuff assets) {
+        this.assets = assets;
+    }
+    public AssetStuff getAssets(){
+        return this.assets;
+    }
 }
 
