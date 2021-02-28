918e20abd3d5596b4e1c10f0de7a074fc43d117a
Mohamedsalbi <73004418+vulture990@users.noreply.github.com>
2021-02-28 20:16:37 +0100

    Update Map.java

--- src/com.vulture/entity/Map.java
+++ src/com.vulture/entity/Map.java
@@ -18,6 +18,7 @@ public class Map {
             for (int y = 0; y < h; y++) {
                 tiles[x][y] = new Tile(Ground.GRASS_1);
                 next[x][y] = new Tile(Ground.GRASS_1);
+                /*
                 Random r = new Random();
                 int rn = r.nextInt(10) + 1;// generating a random number between 1 and 10
                 if (rn > 1) {
@@ -28,121 +29,100 @@ public class Map {
                     tiles[x][y] = new Tile(Ground.GRASS_2);
                     //next[x][y]=new Tile(Ground.GRASS_2);
                 }
-            }
+
+                 */
+
+           }
         }
+       // tiles[5][1].setGround(Ground.GRASS_2);
+    }
+    public void updateMap(float delta){
         //Now time come for conway game of life
         for (int x = 0; x < w; x++) {
             for (int y = 0; y < h; y++) {
-                int check = 0;
-                if (tiles[x][y].getGround() == Ground.GRASS_1) {
-                    //meaning the cell is dead
+                if (x == 0 || x == w - 1 || y == 0 || y == h - 1) {
+                    this.next[x][y].setGround(Ground.GRASS_1);
+                } else {
+                    int check = 0;
+                    if (this.tiles[x][y].getGround() == Ground.GRASS_2) {
+                        //meaning the cell has grass2 meanin it s dead cell
+                        // checking it s neighboors
+                        if (this.tiles[x][y - 1].getGround() == Ground.GRASS_1) {
+                            check++;
 
-                    if (tiles[x][y - 1].getGround() == Ground.GRASS_1) {
-                        check++;
-                        if (x == 0 || x == w - 1 || y == 0 || y == h - 1) {
-                            tiles[x][y].setGround(Ground.GRASS_1);
+                        } else if (this.tiles[x][y + 1].getGround() == Ground.GRASS_1) {
+                            check++;
+
+                        } else if (this.tiles[x][y].getGround() == Ground.GRASS_1) {
+                            continue;
+                        } else if (this.tiles[x - 1][y - 1].getGround() == Ground.GRASS_1) {
+                            check++;
+
+                        } else if (this.tiles[x + 1][y - 1].getGround() == Ground.GRASS_1) {
+                            check++;
+
+                        } else if (this.tiles[x + 1][y + 1].getGround() == Ground.GRASS_1) {
+                            check++;
+
+                        } else if (this.tiles[x + 1][y].getGround() == Ground.GRASS_1) {
+                            check++;
+
+                        } else if (this.tiles[x - 1][y + 1].getGround() == Ground.GRASS_1) {
+                            check++;
+
+                        } else if (this.tiles[x - 1][y - 1].getGround() == Ground.GRASS_1) {
+                            check++;
                         }
-                    } else if (tiles[x][y + 1].getGround() == Ground.GRASS_1) {
-                        check++;
-                        if (x == 0 || x == w - 1 || y == 0 || y == h - 1) {
-                            tiles[x][y].setGround(Ground.GRASS_1);
-                        }
-                    } else if (tiles[x][y].getGround() == Ground.GRASS_1) {
-                        continue;
-                    } else if (tiles[x - 1][y - 1].getGround() == Ground.GRASS_1) {
-                        check++;
-                        if (x == 0 || x == w - 1 || y == 0 || y == h - 1) {
-                            tiles[x][y].setGround(Ground.GRASS_1);
-                        }
-                    } else if (tiles[x + 1][y - 1].getGround() == Ground.GRASS_1) {
-                        check++;
-                        if (x == 0 || x == w - 1 || y == 0 || y == h - 1) {
-                            tiles[x][y].setGround(Ground.GRASS_1);
-                        }
-                    } else if (tiles[x + 1][y + 1].getGround() == Ground.GRASS_1) {
-                        check++;
-                        if (x == 0 || x == w - 1 || y == 0 || y == h - 1) {
-                            tiles[x][y].setGround(Ground.GRASS_1);
-                        }
-                    } else if (tiles[x + 1][y].getGround() == Ground.GRASS_1) {
-                        check++;
-                        if (x == 0 || x == w - 1 || y == 0 || y == h - 1) {
-                            tiles[x][y].setGround(Ground.GRASS_1);
-                        }
-                    } else if (tiles[x - 1][y + 1].getGround() == Ground.GRASS_1) {
-                        check++;
-                        if (x == 0 || x == w - 1 || y == 0 || y == h - 1) {
-                            tiles[x][y].setGround(Ground.GRASS_1);
-                        }
-                    } else if (tiles[x - 1][y - 1].getGround() == Ground.GRASS_1) {
-                        check++;
-                        if (x == 0 || x == w - 1 || y == 0 || y == h - 1) {
-                            tiles[x][y].setGround(Ground.GRASS_1);
-                        }
-                        else continue;
-                        if(check==3){
-                            tiles[x][y].setGround(Ground.GRASS_2);
+
+                        if (check == 3) {
+                            this.next[x][y].setGround(Ground.GRASS_1);
+                            //if 3 of its neighboors  are alive the next generation that cell is going to turn into
                         }
                     } else {//meaning the cell is alive
                         int check2 = 0;
-                        if (tiles[x][y].getGround() == Ground.GRASS_1) {
+                        if (this.tiles[x][y].getGround() == Ground.GRASS_1) {
                             //meaning the cell is dead
 
-                            if (tiles[x][y - 1].getGround() == Ground.GRASS_1) {
+                            if (this.tiles[x][y - 1].getGround() == Ground.GRASS_1) {
                                 check2++;
-                                if (x == 0 || x == w - 1 || y == 0 || y == h - 1) {
-                                    tiles[x][y].setGround(Ground.GRASS_1);
-                                }
-                            } else if (tiles[x][y + 1].getGround() == Ground.GRASS_1) {
+
+                            } else if (this.tiles[x][y + 1].getGround() == Ground.GRASS_1) {
                                 check2++;
-                                if (x == 0 || x == w - 1 || y == 0 || y == h - 1) {
-                                    tiles[x][y].setGround(Ground.GRASS_1);
-                                }
-                            } else if (tiles[x][y].getGround() == Ground.GRASS_1) {
+
+                            } else if (this.tiles[x][y].getGround() == Ground.GRASS_1) {
                                 continue;
-                            } else if (tiles[x - 1][y - 1].getGround() == Ground.GRASS_1) {
+                            } else if (this.tiles[x - 1][y - 1].getGround() == Ground.GRASS_1) {
                                 check2++;
-                                if (x == 0 || x == w - 1 || y == 0 || y == h - 1) {
-                                    tiles[x][y].setGround(Ground.GRASS_1);
-                                }
-                            } else if (tiles[x + 1][y - 1].getGround() == Ground.GRASS_1) {
+
+                            } else if (this.tiles[x + 1][y - 1].getGround() == Ground.GRASS_1) {
                                 check2++;
-                                if (x == 0 || x == w - 1 || y == 0 || y == h - 1) {
-                                    tiles[x][y].setGround(Ground.GRASS_1);
-                                }
-                            } else if (tiles[x + 1][y + 1].getGround() == Ground.GRASS_1) {
+
+                            } else if (this.tiles[x + 1][y + 1].getGround() == Ground.GRASS_1) {
                                 check2++;
-                                if (x == 0 || x == w - 1 || y == 0 || y == h - 1) {
-                                    tiles[x][y].setGround(Ground.GRASS_1);
-                                }
-                            } else if (tiles[x + 1][y].getGround() == Ground.GRASS_1) {
+
+                            } else if (this.tiles[x + 1][y].getGround() == Ground.GRASS_1) {
                                 check2++;
-                                if (x == 0 || x == w - 1 || y == 0 || y == h - 1) {
-                                    tiles[x][y].setGround(Ground.GRASS_1);
-                                }
-                            } else if (tiles[x - 1][y + 1].getGround() == Ground.GRASS_1) {
+
+                            } else if (this.tiles[x - 1][y + 1].getGround() == Ground.GRASS_1) {
                                 check2++;
-                                if (x == 0 || x == w - 1 || y == 0 || y == h - 1) {
-                                    tiles[x][y].setGround(Ground.GRASS_1);
-                                }
-                            } else if (tiles[x - 1][y - 1].getGround() == Ground.GRASS_1) {
+
+                            } else if (this.tiles[x - 1][y - 1].getGround() == Ground.GRASS_1) {
                                 check2++;
-                                if (x == 0 || x == w - 1 || y == 0 || y == h - 1) {
-                                    tiles[x][y].setGround(Ground.GRASS_1);
-                                }
-                                else continue;
-                                if(check2 <2 || check>3){
-                                    tiles[x][y].setGround(Ground.GRASS_1);
-                                }
+                            }
+                            if (check2 < 2 || check2 > 3) {
+                                this.next[x][y].setGround(Ground.GRASS_2);
+                            } else {
+                                this.next[x][y] = this.tiles[x][y];
+
                             }
                         }
                     }
                 }
             }
         }
-        tiles=next;
+        this.tiles=this.next;
+        //tiles=next;
     }
-
     public Tile getTile(int x, int y) {
         return tiles[x][y];
     }
@@ -155,4 +135,4 @@ public class Map {
         return w;
     }
 }
-    
+
