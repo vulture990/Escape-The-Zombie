662bba6330463c1c5f5a109dd1267db16f549cb8
Mohamedsalbi <73004418+vulture990@users.noreply.github.com>
2021-03-10 16:32:21 +0100

    added the hCost(heurstic) & gcost

--- src/com.vulture/entity/Tile.java
+++ src/com.vulture/entity/Tile.java
@@ -5,14 +5,25 @@ package com.vulture.entity;
 
 import com.vulture.Assets.AssetStuff;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tile  {
     private Ground gr;
     private Player player;
     private Enemy enemy;
     private AssetStuff assets;
+    private int gCost;
+    private int hCost;
+    public Tile previous;
+    private int fCost=gCost+hCost;
+    public int xposMap;
+    public  int yposMap;
+    public List<Tile> neighbors=new ArrayList<Tile>();
   //  private AssetStuff objects;
     public Tile(Ground gr){
         this.gr = gr;
+
     }
     public  Ground getGround()
     { return gr;
@@ -32,12 +43,57 @@ public class Tile  {
     public void setEnemy(Enemy enemy){
         this.enemy=enemy;
     }
+    public void setgCost(int g){
+        this.gCost=g;
+    }
+    public int getXposMap(){
+        return this.xposMap;
+    }
+    public void setXposMap(int x){
+        this.xposMap=x;
+    }
+    public int getYposMap(){
+        return this.yposMap;
+    }
 
+    public void setfCost(int fCost) {
+        this.fCost = fCost;
+    }
+    public int getfCost(){
+        return fCost;
+    }
+
+    public void setYposMap(int y){
+        this.yposMap=y;
+    }
+    public int getgCost(){
+        return this.gCost;
+    }
+    public int gethCost(){
+        return this.hCost;
+    }
+    public  void sethCost(int h){
+        this.hCost=h;
+    }
     public void setAssets(AssetStuff assets) {
         this.assets = assets;
     }
     public AssetStuff getAssets(){
         return this.assets;
+    }
+    public void addNeighboors(Map map){
+        if(this.getXposMap()<map.getW()-1) {
+            this.neighbors.add(map.getTile(this.getXposMap() + 1, this.getYposMap()));
+        }
+        if(this.getXposMap()>0) {
+            this.neighbors.add(map.getTile(this.getXposMap() - 1, this.getYposMap()));
+        }
+        if(this.getYposMap()<map.getH()-1) {
+            this.neighbors.add(map.getTile(this.getXposMap(), this.getYposMap() + 1));
+        }
+        if(this.getYposMap()>0) {
+            this.neighbors.add(map.getTile(this.getXposMap(), this.getYposMap() - 1));
+        }
     }
 }
 
