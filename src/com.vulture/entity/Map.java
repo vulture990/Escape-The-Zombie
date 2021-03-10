5e454b4529edac4f355021c206de6447bef6fc48
Mohamedsalbi <73004418+vulture990@users.noreply.github.com>
2021-03-10 16:31:23 +0100

    Update Map.java

--- src/com.vulture/entity/Map.java
+++ src/com.vulture/entity/Map.java
@@ -1,37 +1,39 @@
 package com.vulture.entity;
 import java.util.Random;
-
 // we gonna process the classic way 2d array
 public class Map {
     private int w, h;
     private Tile[][] tiles;
-    private Tile[][] next;
+
     public Map(int w, int h) {
         this.w = w;
         this.h = h;
         tiles = new Tile[w][h];
-        next = new Tile[w][h];
         //init for each tile object
         for (int x = 0; x < w; x++) {
             for (int y = 0; y < h; y++) {
                 //tiles[x][y] = new Tile(Ground.GRASS_1);
-
                 Random r = new Random();
                 int rn = r.nextInt(10) + 1;// generating a random number between 1 and 10
                 if (rn > 1) {
                         tiles[x][y] = new Tile(Ground.GRASS_1);
-                    //next[x][y]=new Tile(Ground.GRASS_1);
-
+                        tiles[x][y].setXposMap(x);
+                        tiles[x][y].setYposMap(y);
                 } else {
                     tiles[x][y] = new Tile(Ground.GRASS_2);
-                    //next[x][y]=new Tile(Ground.GRASS_2);
+                    tiles[x][y].setXposMap(x);
+                    tiles[x][y].setYposMap(y);
                 }
            }
         }
-        //tiles[5][5].setGround(Ground.GRASS_2);
     }
-
-
+    public void init() {
+        for (int x = 0; x < w; x++) {
+            for (int y = 0; y < h; y++) {
+                tiles[x][y].addNeighboors(this);
+            }
+        }
+    }
 
 
     public Tile getTile(int x, int y) {
